eac9d68ed62857d649e7909359bfd30831651b01
unknown <cskeith01@gmail.com>
2022-04-20 11:40:01 +0200

    Add Footer info

--- coflix/src/Components/Footer.js
+++ coflix/src/Components/Footer.js
@@ -6,8 +6,17 @@ function Footer() {
   return (
     <>
     <div className='footer'>
-      <div className='sponsors'>Sponsors</div>
-      <div className='conactInfo'>Contact Info</div>
+      <div className='sponsors'>
+        <p>Powerd by</p>
+        <p>BEcode</p>
+        <p>VDAB</p>
+
+      </div>
+      <div className='conactInfo'>
+        <p>Contact Info</p>
+        <p>Colin Renkema</p>
+        <p>cskeith01@gmail.com</p>
+      </div>
     </div>
     </>
   )
