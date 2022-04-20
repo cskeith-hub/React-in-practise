b264ed89bf5fde2ba98f84207f83638884d14920
unknown <cskeith01@gmail.com>
2022-04-20 13:57:43 +0200

    WIP movie API

--- coflix/src/App.js
+++ coflix/src/App.js
@@ -1,12 +1,15 @@
 import Header from "./Components/Header";
+import MovieList from "./Components/MovieList";
 import Footer from "./Components/Footer";
 
 import "./Styles/style.css"
+
 
 function App() {
   return (
    <>
    <Header />
+   <MovieList />
    <Footer />
    </>
   );
---
+++ coflix/src/Components/MovieList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function MovieList() {
+  return (
+    <>
+    <div>
+      Show me some movies
+    </div>
+    </>
+   
+
+  )
+}
+
+export default MovieList
