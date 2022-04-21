42272be680c45581bf066a30133a42d9de68e903
unknown <cskeith01@gmail.com>
2022-04-21 11:39:17 +0200

    Map Movies

--- coflix/src/Components/DataFetching.js
+++ coflix/src/Components/DataFetching.js
@@ -1,23 +1,25 @@
 import {useState, useEffect } from "react";
 import apiKey from "../Config";
+import MovieCard from "./MovieCard";
 
 function DataFetching() {
-    const [Movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([])
     useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/550?api_key='+ apiKey.movieKey)
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key='+ apiKey.movieKey)
          .then((data) => data.json())
          .then(data => {
-            console.log(data)
-            setMovies(data)
+            setMovies(data.results)
          })
        }, []);
-
+       console.log(movies)
   return (
-    <div>
-        {Movies.title}
-        {Movies.release_date}
+    <div className="Movies">
+        {movies.map(movie =>(
+            <div className="movieTitle">{movie.title}</div>
+            
+        ))}
     </div>
-  )
+    );
 }
 
 export default DataFetching
